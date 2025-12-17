979b3a77f37bc2e995c0171d1ec7e6bae6e82692
patejdlstepan <patejdlstepan@sssvt.cz>
2025-12-17 13:29:23 +0100

    adasdasdasdsa

--- boty-asp/Areas/Admin/Controllers/ColorAdminController.cs
+++ boty-asp/Areas/Admin/Controllers/ColorAdminController.cs
@@ -1,5 +1,6 @@
 using boty_asp.Areas.Admin.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace boty_asp.Areas.Admin.Controllers;
 
@@ -27,7 +28,7 @@ public class ColorAdminController : Controller {
             return NotFound();
         }
         
-        var color = _context.Colors.FirstOrDefault(c => c.Id == id);
+        var color = _context.Colors.Include(c => c.ProductVariants).FirstOrDefault(c => c.Id == id);
 
         if (color == null)
         {
@@ -38,8 +39,41 @@ public class ColorAdminController : Controller {
     }
     
     [Area("Admin")]
-    [HttpPost, ActionName("Delete")]
-    public IActionResult Delete(int id)
+    public IActionResult DeleteSize(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        
+        var size = _context.Sizes.Include(c => c.ProductVariants).FirstOrDefault(c => c.Id == id);
+
+        if (size == null)
+        {
+            return NotFound();
+        }
+
+        return View(size);
+    }
+    
+    [Area("Admin")]
+    [HttpPost, ActionName("Delete1")]
+    public IActionResult Delete1(int id)
+    {
+        var color = _context.Colors.Find(id);
+    
+        if (color != null)
+        {
+            _context.Colors.Remove(color); 
+            _context.SaveChanges();
+        }
+    
+        return RedirectToAction(nameof(Index));
+    }
+    
+    [Area("Admin")]
+    [HttpPost, ActionName("Delete2")]
+    public IActionResult Delete2(int id)
     {
         var color = _context.Colors.Find(id);
     
